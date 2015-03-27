f3f61bdb6f380944fb6ea1377ff1ceea9d52ee66
Yurii Tsap <famaous@ukr.net>
2015-03-27 11:33:14 +0200

    some updates

--- app/src/main/java/globallogic/yuriitsap/com/customlayout/CustomLayout.java
+++ app/src/main/java/globallogic/yuriitsap/com/customlayout/CustomLayout.java
@@ -14,10 +14,11 @@ import java.util.Random;
  */
 public class CustomLayout extends ViewGroup {
 
-    private ViewGroup.LayoutParams mLayoutParams;
+    private AttributeSet mAttributeSet;
 
     public CustomLayout(Context context, AttributeSet attr) {
         super(context, attr);
+        mAttributeSet = attr;
 
     }
 
@@ -30,15 +31,18 @@ public class CustomLayout extends ViewGroup {
 
         setMeasuredDimension(measuredWidth, heightMeasureSpec);
 
-
         for (int i = getChildCount() - 1; i >= 0; --i) {
             View child = getChildAt(i);
+            int widthMode = MeasureSpec.AT_MOST;
+            int heightMode = MeasureSpec.AT_MOST;
             child.setLayoutParams(generateDefaultLayoutParams());
-            int widthMode = MeasureSpec.EXACTLY;
-            int heightMode = MeasureSpec.EXACTLY;
-            int newChildWidthMeasureSpec = MeasureSpec.makeMeasureSpec(child.getLayoutParams().width, widthMode);
-            int newChildHeightMeasureSpec = MeasureSpec.makeMeasureSpec(child.getLayoutParams().width, heightMode);
+            int newChildWidthMeasureSpec = MeasureSpec
+                    .makeMeasureSpec(measuredWidth, widthMode);
+            int newChildHeightMeasureSpec = MeasureSpec
+                    .makeMeasureSpec(measuredHeight, heightMode);
             child.measure(newChildWidthMeasureSpec, newChildHeightMeasureSpec);
+            ((LayoutParams) child.getLayoutParams()).setSizeMeasured(true);
+
         }
 
     }
@@ -54,17 +58,18 @@ public class CustomLayout extends ViewGroup {
 
         for (int i = getChildCount() - 1; i >= 0; i--) {
             View child = getChildAt(i);
-            if (child.getVisibility() != GONE) {
-
-                int childWidth = child.getMeasuredWidth();
-                int childHeight = child.getMeasuredHeight();
-                int currentLeft = random.nextInt(parenRight - childWidth - parentLeft) + parentLeft;
-                int currentTop = random.nextInt(parentBottom - childHeight - parentTop) + parentTop;
-                int currentRight = currentLeft + childWidth;
-                int currentBottom = currentTop + childHeight;
-                child.layout(currentLeft, currentTop, currentRight, currentBottom);
-
+            if (((LayoutParams) child.getLayoutParams()).isSizeMeasured()) {
+                continue;
             }
+
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
+            int currentLeft = random.nextInt(parenRight - childWidth - parentLeft) + parentLeft;
+            int currentTop = random.nextInt(parentBottom - childHeight - parentTop) + parentTop;
+            int currentRight = currentLeft + childWidth;
+            int currentBottom = currentTop + childHeight;
+            child.layout(currentLeft, currentTop, currentRight, currentBottom);
+
 
         }
     }
@@ -81,10 +86,8 @@ public class CustomLayout extends ViewGroup {
 
     @Override
     protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
-        int childWidth = this.getMeasuredWidth()/getChildCount();
-        int childHeight = this.getMeasuredHeight()/getChildCount();
-        boolean measured = true;
-        return new LayoutParams(childWidth,childHeight,measured);
+        return new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 
     @Override
@@ -122,39 +125,35 @@ public class CustomLayout extends ViewGroup {
 
     public static class LayoutParams extends ViewGroup.LayoutParams {
 
-        public int height;
-        public int width;
+
         public boolean sizeMeasured;
 
-        /**
-         * Continue,Break,Return - done
-         * implement own logic of measuring views without helper functions - done
-         * code convention really needed
-         * UI overview
-         * layout params with boolean
-         * workflow of parent and child
-         * we don't need seting layout params out of the view
-         * x,y != width,height
-         * Layout must be random
-         */
 
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
 
         public LayoutParams(int width, int height, boolean measured) {
             super(width, height);
-            this.width = width;
-            this.height = height;
             this.sizeMeasured = measured;
         }
 
         public LayoutParams(ViewGroup.LayoutParams layoutParams) {
             super(layoutParams);
-            this.width = layoutParams.width;
-            this.height = layoutParams.height;
+
 
         }
 
         public LayoutParams(Context c, AttributeSet attrs) {
             super(c, attrs);
+        }
+
+        public boolean isSizeMeasured() {
+            return sizeMeasured;
+        }
+
+        public void setSizeMeasured(boolean sizeMeasured) {
+            this.sizeMeasured = sizeMeasured;
         }
     }
 }
